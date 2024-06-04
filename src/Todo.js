3071bcfb4b905f36ba7731780597f04b038536fb
pyy2114 <pyy2114@naver.com>
2024-06-05 03:17:32 +0900

    디데이 기능 추가

--- src/Todo.js
+++ src/Todo.js
@@ -1,76 +1,78 @@
 import React from "react";
-import {ListItem, ListItemText, InputBase, Checkbox, ListItemSecondaryAction, IconButton}from "@material-ui/core";
+import { ListItem, ListItemText, InputBase, Checkbox, ListItemSecondaryAction, IconButton } from "@material-ui/core";
 import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
-class Todo extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={item: props.item, readOnly:true};  //매개변수 item의 변수/값을 item에 대입
-        this.delete=props.delete;
-        this.update = props.update;
-    }
 
-    deleteEventHandler =()=>{
-        this.delete(this.state.item);
-    }
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { item: props.item, readOnly: true };  // 매개변수 item의 변수/값을 item에 대입
+    this.delete = props.delete;
+    this.update = props.update;
+  }
 
-    offReadOnlyMode =()=>{
-        this.setState({readOnly:false},()=>{
-            console.log("ReadOnly?",this.state.readOnly)
-        });
-    }
+  deleteEventHandler = () => {
+    this.delete(this.state.item);
+  }
 
-    enterKeyEventHandler =(e)=>{
-        if(e.key ==="Enter"){
-            this.setState({readOnly:true})
-            this.update(this.state.item);
-        }
-    }
+  offReadOnlyMode = () => {
+    this.setState({ readOnly: false }, () => {
+      console.log("ReadOnly?", this.state.readOnly)
+    });
+  }
 
-    editEventHandler =(e)=>{
-        const thisItem =this.state.item;
-        thisItem.title = e.target.value;
-        this.setState({item:thisItem});
+  enterKeyEventHandler = (e) => {
+    if (e.key === "Enter") {
+      this.setState({ readOnly: true })
+      this.update(this.state.item);
     }
+  }
 
-    checkboxEventHandler =(e)=>{
-        const thisItem = this.state.item;
-        thisItem.done = thisItem.done ?false :true;
-        //this.setState({item:thisItem});
-        this.setState({readOnly:true});
-        this.update(this.state.item);
-    }
-    render(){
-        const item = this.state.item;
-        return(
-            <ListItem>
-                <Checkbox 
-                    checked={item.done}
-                    onChange={this.checkboxEventHandler}
-                />
-                <ListItemText>
-                    <InputBase
-                        inputProps={{"aria-label":"naked", readOnly:this.state.readOnly}}
-                        type="text"
-                        id={item.id}
-                        name={item.id}
-                        value={item.title}
-                        multiline={true}
-                        fullWidth={true}
-                        onClick={this.offReadOnlyMode}
-                        onChange={this.editEventHandler}
-                        onKeyPress={this.enterKeyEventHandler}
-                    />
-                </ListItemText>
+  editEventHandler = (e) => {
+    const thisItem = this.state.item;
+    thisItem.title = e.target.value;
+    this.setState({ item: thisItem });
+  }
 
-                <ListItemSecondaryAction>
-                    <IconButton aria-label="Delete"
-                    onClick={this.deleteEventHandler}>
-                        <DeleteOutlined />
-                    </IconButton>
-                </ListItemSecondaryAction>
-            </ListItem>
-        );
-    }
+  checkboxEventHandler = (e) => {
+    const thisItem = this.state.item;
+    thisItem.done = !thisItem.done;
+    this.setState({ readOnly: true });
+    this.update(this.state.item);
+  }
+
+  render() {
+    const item = this.state.item;
+    return (
+      <ListItem>
+        <Checkbox
+          checked={item.done}
+          onChange={this.checkboxEventHandler}
+        />
+        <ListItemText>
+          <InputBase
+            inputProps={{ "aria-label": "naked", readOnly: this.state.readOnly }}
+            type="text"
+            id={item.id}
+            name={item.id}
+            value={item.title}
+            multiline={true}
+            fullWidth={true}
+            onClick={this.offReadOnlyMode}
+            onChange={this.editEventHandler}
+            onKeyPress={this.enterKeyEventHandler}
+            style={{ textDecoration: item.done ? 'line-through' : 'none' }} // 줄 긋기 스타일 추가
+          />
+        </ListItemText>
+
+        <ListItemSecondaryAction>
+          <IconButton aria-label="Delete"
+            onClick={this.deleteEventHandler}>
+            <DeleteOutlined />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    );
+  }
 }
 
 export default Todo;
